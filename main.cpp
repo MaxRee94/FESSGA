2d7e6e57a6ebe44949fa4472b8a6151f6903c737
Max van der Ree <maxvdree@hotmail.com>
2022-12-31 10:37:47 +0100

    Fix numerical instability

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <igl/opengl/glfw/Viewer.h>
 
 using namespace Eigen;
-
+using namespace std;
 
 int main(int argc, char *argv[])
 {
@@ -15,7 +15,8 @@ int main(int argc, char *argv[])
     1.0,0.0,1.0,
     1.0,1.0,0.0,
     1.0,1.0,1.0).finished();
-  MatrixXd V_homog = V.rowwise().homogeneous();
+  MatrixXd V_homog_orig = V.rowwise().homogeneous();
+  MatrixXd V_homog = V_homog_orig;
   const MatrixXi F = (MatrixXi(12,3)<<
     0,6,4,
     0,2,6,
@@ -34,11 +35,11 @@ int main(int argc, char *argv[])
   
   Vector3d transform = Vector3d(0.001f, 0, 0);
   Matrix4d T = (Matrix4d(4, 4) <<
-      1.0, 0.0, 0.0, transform(0),
-      0.0, 1.0, 0.0, transform(1),
-      0.0, 0.0, 1.0, transform(2),
-      0.0, 0.0, 0.0, 1.0).finished();
-  //T = T.transpose();
+      1.0, 0.0, 0.0, 0.0,
+      0.0, 1.0, 0.0, 0.0,
+      0.0, 0.0, 1.0, 0.0,
+      transform(0), transform(1), transform(2), 1.0).finished();
+  T = T.transpose();
 
   // Set initial cube mesh
   viewer.data().set_mesh(V, F);
@@ -46,13 +47,26 @@ int main(int argc, char *argv[])
 
   viewer.core().is_animating = true;
 
+  MatrixXd V_homog_prev;
+  Vector3d d_transform = transform;
   viewer.callback_pre_draw = [&](igl::opengl::glfw::Viewer&)->bool
   {
+      // Update transform matrix
+      transform += d_transform;
+      T(3, 0) = transform(0);
+      T(3, 1) = transform(1);
+      T(3, 2) = transform(2);
+
       viewer.data().clear();
       viewer.data().set_mesh(V, F);
       viewer.data().set_face_based(true);
-      V_homog *= T;
+      V_homog_prev = V_homog;
+      V_homog = V_homog_orig * T;
       V = V_homog.rowwise().hnormalized();
+      cout << "V homologous: \n" << V_homog << "\n" << endl;
+      cout << "V: \n" << V << "\n" << endl;
+      cout << "Change: \n" << (V_homog - V_homog_prev) << "\n" << endl;
+
       return false;
   };
   viewer.launch();
