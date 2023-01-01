74c3af3194d6f8da06e46b6c92d716868dbf8f55
Max van der Ree <maxvdree@hotmail.com>
2023-01-01 18:58:40 +0100

    Bind animation start/hold to spacebar keypress event

--- main.cpp
+++ main.cpp
@@ -3,6 +3,43 @@
 using namespace Eigen;
 using namespace std;
 
+bool animation = false;
+
+void transform(
+    igl::opengl::glfw::Viewer& viewer, MatrixXd& V_homog_orig, MatrixXd& V_homog, Matrix4d& T,
+    MatrixXd& V, MatrixXi F, Vector3d& pos_offset, Vector3d& transl
+) {
+
+    // Update transform matrix
+    pos_offset += transl;
+    T(3, 0) = pos_offset(0);
+    T(3, 1) = pos_offset(1);
+    T(3, 2) = pos_offset(2);
+
+    // Transform mesh
+    V_homog = V_homog_orig * T;
+    V = V_homog.rowwise().hnormalized();
+
+    // Update viewer
+    viewer.data().clear();
+    viewer.data().set_mesh(V, F);
+    viewer.data().set_face_based(true);
+}
+
+
+// This function is called every time a keyboard button is pressed
+bool key_down(igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier)
+{
+    //std::cout << "Key: " << key << " " << (unsigned int)key << std::endl;
+    if (key == 32) { // Spacebar
+        animation = !animation;
+        viewer.core().is_animating = animation;
+        if (animation) cout << "Started animation" << endl;
+        else cout << "Paused animation" << endl;
+    }
+    return false;
+}
+
 int main(int argc, char *argv[])
 {
   // Inline mesh of a cube
@@ -35,33 +72,26 @@ int main(int argc, char *argv[])
   Vector3d pos_offset = Vector3d(0, 0, 0);
   Vector3d transl = Vector3d(0.001f, 0, 0);
   Matrix4d T = (Matrix4d(4, 4) <<
-      1.0,          0.0,        0.0,        0.0,
-      0.0,          1.0,        0.0,        0.0,
-      0.0,          0.0,        1.0,        0.0,
-      transl(0),    transl(1),  transl(2),  1.0).finished();
+      1,          0,        0,        0,
+      0,          1,        0,        0,
+      0,          0,        1,        0,
+      0,          0,        0,        1).finished();
   T = T.transpose();
 
   // Initialize viewer
   igl::opengl::glfw::Viewer viewer;
-  viewer.core().is_animating = true;
+  
+  // Update viewer
+  viewer.data().clear();
+  viewer.data().set_mesh(V, F);
+  viewer.data().set_face_based(true);
+
+  viewer.callback_key_down = &key_down;
   viewer.callback_pre_draw = [&](igl::opengl::glfw::Viewer&)->bool
   {
-      // Update transform matrix
-      pos_offset += transl;
-      T(3, 0) = pos_offset(0);
-      T(3, 1) = pos_offset(1);
-      T(3, 2) = pos_offset(2);
-
-      // Transform mesh
-      V_homog = V_homog_orig * T;
-      V = V_homog.rowwise().hnormalized();
-
-      // Update viewer
-      viewer.data().clear();
-      viewer.data().set_mesh(V, F);
-      viewer.data().set_face_based(true);
-
+      if (animation) transform(viewer, V_homog_orig, V_homog, T, V, F, pos_offset, transl);
       return false;
   };
+  //viewer.callback_pre_draw = [&]->bool {transform(viewer, V_homog_orig, V_homog, T, V, F, pos_offset, transl};
   viewer.launch();
 }
