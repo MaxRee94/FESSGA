42bb02e5cef78c841bb412e9b2d9407c65b558aa
Max van der Ree <maxvdree@hotmail.com>
2023-01-02 00:08:55 +0100

    Add offset between each mesh (not working yet)

---
+++ gui.cpp
@@ -0,0 +1,20 @@
+#include <igl/opengl/glfw/Viewer.h>
+#include "gui.h"
+
+mvis::GUI::GUI() {
+    //// Initialize viewer
+    //igl::opengl::glfw::Viewer viewer;
+    //viewer.data().set_mesh(V_list[0], F_list[0]);
+    //viewer.data().set_face_based(true);
+
+    //viewer.callback_key_down = &key_down;
+    //viewer.callback_pre_draw = [&](igl::opengl::glfw::Viewer&)->bool
+    //{
+    //    if (viewer.core().is_animating) transform(
+    //        viewer, Vhom_orig, Vhom, T, V_list[0], F_list[0], pos_offset, transl, rot_y_offset, rot_y_speed
+    //    );
+    //    return false;
+    //};
+    //viewer.launch();
+}
+
--- gui.h
+++ gui.h
@@ -1,8 +1,10 @@
 #pragma once
 #include <iostream>
+#include <igl/readOFF.h>
 
 namespace mvis {
 	class GUI {
+		GUI();
 	public:
 
 	};
