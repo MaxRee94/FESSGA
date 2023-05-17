13391b9def9bb8308ed49ed49a204bf091d6d7aa
Max van der Ree <maxvdree@hotmail.com>
2023-05-17 18:15:48 +0200

    Add evolver class, including 2d crossover function

---
+++ evolver.cpp
@@ -0,0 +1,33 @@
+#include "evolver.h"
+#include <iostream>
+
+
+void Evolver::do_2d_crossover(uint* parent1, uint* parent2, uint* child1, uint* child2, int dim_x, int dim_y) {
+	int domain_size = dim_x * dim_y;
+	vector<uint> crosspoints = { mvis::help::get_rand_uint(0, domain_size), mvis::help::get_rand_uint(0, domain_size) };
+	uint crosspoint_1 = min(crosspoints[0], crosspoints[1]);
+	uint crosspoint_2 = max(crosspoints[0], crosspoints[1]);
+	//cout << "crosspoint 1: " << crosspoint_1 << ", crosspoint 2: " << crosspoint_2 << endl;
+	for (int x = 0; x < dim_x; x++) {
+		for (int y = 0; y < dim_y; y++) {
+			int coord = x * dim_x + y;
+			if (coord > crosspoint_1 && coord < crosspoint_2) {
+				child1[coord] = parent1[coord];
+				child2[coord] = parent2[coord];
+			}
+			else {
+				child1[coord] = parent2[coord];
+				child2[coord] = parent1[coord];
+			}
+		}
+	}
+}
+
+void Evolver::init_population() {
+	population = new uint[pop_size * dim_x * dim_y * dim_z];
+}
+
+void Evolver::do_evolution() {
+
+}
+
---
+++ evolver.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <Eigen/Core>
+#include <algorithm>
+#include <map>
+#include "Helpers.h"
+#include <functional>
+
+
+class Evolver {
+public:
+	Evolver() = default;
+	Evolver(
+		int _pop_size, float _mutation_rate, function<bool(uint*, int)> _terminate,
+		int _tournament_size, int _dim_x, int _dim_y, int _dim_z = 0
+	) {
+		dim_x = _dim_x;
+		dim_y = _dim_y;
+		dim_z = _dim_z;
+		if (dim_z == 0) {
+			dim_z = 1; domain_2d = true;
+		}
+		pop_size = _pop_size;
+		mutation_rate = _mutation_rate;
+		tournament_size = _tournament_size;
+		terminate = _terminate;
+	}
+	void do_2d_crossover(uint* parent1, uint* parent2, uint* child1, uint* child2, int dim_x, int dim_y);
+	void init_population();
+	void do_evolution();
+private:
+	int dim_x = 1;
+	int dim_y = 1;
+	int dim_z = 1;
+	bool domain_2d = false;
+	int pop_size = 1;
+	float mutation_rate = 0;
+	int tournament_size = 1;
+	uint* population = 0;
+	function<bool(uint*, int)> terminate = 0;
+};
+
