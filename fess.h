3def449f7d7a498583a7d408aaaf4475b410eb86
Max van der Ree <maxvdree@hotmail.com>
2023-06-09 00:23:57 +0200

    Add physics reading to FESS + improve logging

--- fess.h
+++ fess.h
@@ -29,48 +29,53 @@ void FESS::run() {
 	cout << "Beginning FESS run. Saving results to " << output_folder << endl;
 
 	double min_stress = 1.0;
+	double max_stress = 1e8;
 	string msh = msh_file;
 	string cur_iteration_name = "";
 	string cur_output_folder = output_folder;
 	int i = 0;
-	while (min_stress > min_stress_threshold) {
-		cout << "\n--- Starting FESS iteration " << i << ".\n";
+	while (max_stress < max_stress_threshold) {
+		cout << "\nFESS: Starting iteration " << i << ".\n";
 
 		// Create new subfolder for output of current iteration
 		string cur_iteration_name = fessga::help::add_padding("iteration_", i + 1) + to_string(i + 1);
 		string cur_output_folder = output_folder + "/" + cur_iteration_name;
-		cout << "Created output folder " << cur_output_folder << " for current iteration.\n";
+		cout << "FESS: Created output folder " << cur_output_folder << " for current iteration.\n";
 		IO::create_folder_if_not_exists(cur_output_folder);
 
 		// Copy the case.sif file to the newly created subfolder
 		IO::copy_file(case_file, cur_output_folder + "/case.sif");
-		if (IO::FileExists(cur_output_folder + "/case.sif")) cout << "Copied case file to subfolder.\n";
-		else cout << "ERROR: Failed to copy case file to subfolder.\n";
+		if (IO::FileExists(cur_output_folder + "/case.sif")) cout << "FESS: Copied case file to subfolder.\n";
+		else cout << "FESS: ERROR: Failed to copy case file to subfolder.\n";
 		
 		// Generate new FE mesh using modified density distribution
-		cout << "Generating new FE mesh...\n";
+		cout << "FESS: Generating new FE mesh...\n";
 		mesher::FEMesh2D fe_mesh;
 		mesher::generate_FE_mesh(grid, mesh, densities, fe_mesh);
-		cout << "FE mesh generation done.\n";
+		cout << "FESS: FE mesh generation done.\n";
 
 		// Export newly generated FE mesh
 		string bat_file = mesher::export_as_elmer_files(&fe_mesh, cur_output_folder);
-		if (IO::FileExists(cur_output_folder + "/mesh.header")) cout << "Exported new FE mesh to subfolder.\n";
-		else cout << "ERROR: Failed to export new FE mesh to subfolder.\n";
+		if (IO::FileExists(cur_output_folder + "/mesh.header")) cout << "FESS: Exported new FE mesh to subfolder.\n";
+		else cout << "FESS: ERROR: Failed to export new FE mesh to subfolder.\n";
 
 		// Call Elmer to run FEA on new FE mesh
-		cout << "Calling Elmer .bat file...\n";
+		cout << "FESS: Calling Elmer .bat file (" << bat_file << ")\n";
 		physics::call_elmer(bat_file);
 
-		// Wait for Elmer's analysis to complete. This is the case when a new .vtk file has appeared
-
 		// Obtain stress distribution from the .vtk file
-		min_stress = 0.0;
+		Vector2d inv_cell_size = Vector2d(1.0 / grid.cell_size(0), 1.0 / grid.cell_size(1));
+		Vector2d offset = Vector2d(mesh.offset(0), mesh.offset(1));
+		double* vonmises = new double[(grid.x + 1) * (grid.y + 1)]; // Nodes grid has +1 width along each dimension
+		string filename = cur_output_folder + "/case0001.vtk";
+		physics::load_2d_physics_data(filename, vonmises, grid.x + 1, grid.y + 1, offset, inv_cell_size);
+		cout << "FESS: Finished reading physics data." << endl;
 
 		// Set all cells that have corresponding stress values lower than <min_stress_threshold> to density=0.
+		max_stress = 1e10; // Temporary value that will make FESS terminate after one iteration. TODO: remove
 
 		i++;
 	}
 
-	cout << "\nFinished FESS run. Results were saved to " << output_folder << endl;
+	cout << "FESS: Algorithm converged. Results were saved to " << output_folder << endl;
 }
